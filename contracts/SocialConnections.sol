fc7b492fcb8a043a47355bbd04a5b33da05c7af8
Igor Shadurin <igor.shadurin@gmail.com>
2024-11-27 14:32:59 +0300

    feat: add events for social connections

--- contracts/SocialConnections.sol
+++ contracts/SocialConnections.sol
@@ -6,6 +6,15 @@ pragma solidity ^0.8.20;
  * @dev Store and manage social connections (in form of Multihash) for users and services.
  */
 contract SocialConnections {
+    // Event for setting a service connection
+    event ServiceConnectionSet(address indexed serviceAddress, bytes32 hash, uint8 hashFunction, uint8 size);
+
+    // Event for setting a user connection
+    event UserConnectionSet(address indexed userAddress, bytes32 hash, uint8 hashFunction, uint8 size);
+
+    // Event for removing a connection
+    event ConnectionRemoved(address indexed accountAddress, bool isService);
+
     struct Multihash {
         bytes32 hash;
         uint8 hashFunction;
@@ -24,6 +33,7 @@ contract SocialConnections {
      */
     function setServiceConnection(Multihash memory multihash) public {
         serviceConnections[msg.sender] = multihash;
+        emit ServiceConnectionSet(msg.sender, multihash.hash, multihash.hashFunction, multihash.size);
     }
 
     /**
@@ -32,17 +42,20 @@ contract SocialConnections {
      */
     function setUserConnection(Multihash memory multihash) public {
         userConnections[msg.sender] = multihash;
+        emit UserConnectionSet(msg.sender, multihash.hash, multihash.hashFunction, multihash.size);
     }
 
     /**
-     * @dev Delete the Multihash associated with the sender's smart account.
+     * @dev Remove the Multihash associated with the sender's smart account.
      * @param isService Indicates whether the sender's account is a service account.
      */
-    function deleteConnection(bool isService) public {
+    function removeConnection(bool isService) public {
         if (isService) {
             delete serviceConnections[msg.sender];
         } else {
             delete userConnections[msg.sender];
         }
+
+        emit ConnectionRemoved(msg.sender, isService);
     }
 }
